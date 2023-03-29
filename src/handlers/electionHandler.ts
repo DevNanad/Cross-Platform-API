0cc14c6b1457a9b1ad4f57cd41da4bd6bf9c0005
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-03-29 17:39:47 +0800

    add connect id org & elec

--- src/handlers/electionHandler.ts
+++ src/handlers/electionHandler.ts
@@ -8,12 +8,34 @@ export const createElection = async (req, res) => {
             data: {
                 title: req.body.title,
                 startDate: new Date(req.body.startDate),
-                endDate: new Date(req.body.endDate)
+                endDate: new Date(req.body.endDate),
             }
         })
         res.json(election)
     } catch (error) {
         console.error(error)
         res.status(400).json({error})
+    }
+}
+
+//connect an organization to election
+export const connectOrg = async (req, res) => {
+    try {
+        const connectOrgToElection = await prisma.election.update({
+            where:{
+                id: req.body.election_id
+            },
+            data: {
+                organizations: {
+                    connect: {
+                        id: req.body.org_id
+                    }
+                }
+            }
+        })
+        res.json(connectOrgToElection)
+    } catch (error) {
+        console.error(error)
+        res.status(400).json({error})       
     }
 }
--- src/routers/electionRoute.ts
+++ src/routers/electionRoute.ts
@@ -1,10 +1,12 @@
 import {Router} from 'express'
-import { createElection } from '../handlers/electionHandler'
+import { createElection, connectOrg } from '../handlers/electionHandler'
 import { validateRequestSchema } from '../modules/validate-request-schema'
 import { electionSchema } from '../validators/electionSchema'
 
 const router = Router()
 
 router.post('/create', electionSchema, validateRequestSchema, createElection)
+
+router.patch('/connectorg', connectOrg)
 
 export default router
