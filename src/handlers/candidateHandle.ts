3431089baee354a0901facf5fdd5d7e2afcb48ae
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-03-31 19:56:41 +0800

    add connect id candidate & seat

--- src/handlers/candidateHandle.ts
+++ src/handlers/candidateHandle.ts
@@ -18,3 +18,45 @@ export const createCandidate = async (req, res) => {
         res.status(400).json({error})
     }
 }
+
+export const candidateToSeat = async (req, res) => {
+    try {
+
+        //check if the passed ballot id exists in the database
+        const seatExists = await prisma.seat.findUnique({
+            where: {
+                id: req.body.seat_id
+            }
+          })
+        
+        if(!seatExists) throw new Error("Seat not found");
+        
+        //check if the passed seat id exists in the database
+        const candidateExists = await prisma.candidate.findUnique({
+            where: {
+                id: req.body.candidate_id
+            }
+          })
+        
+        if(!candidateExists) throw new Error("Candidate not found");
+        
+
+
+        const connectCandidateToSeat = await prisma.seat.update({
+            where:{
+                id: req.body.seat_id
+            },
+            data: {
+                candidates: {
+                    connect: {
+                        id: req.body.candidate_id
+                    }
+                }
+            }
+        })
+        res.json(connectCandidateToSeat)
+    } catch (error) {
+        console.error(error.message)
+        res.status(400).json({error: error.message})       
+    }
+}
--- src/routers/candidateRoute.ts
+++ src/routers/candidateRoute.ts
@@ -1,8 +1,9 @@
 import {Router} from 'express'
-import { createCandidate } from '../handlers/candidateHandle'
+import { candidateToSeat, createCandidate } from '../handlers/candidateHandle'
 
 const router = Router()
 
 router.post('/create', createCandidate)
 
+router.patch('/connectcandidate', candidateToSeat)
 export default router
