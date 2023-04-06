6b59cd47104179fd1d1ee02e9b1ccbc45ef86ea4
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-04-06 23:58:54 +0800

    add cronjob exec every 1hour

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 import app from './server'
+import cronjob from 'node-cron'
+import { updateExpiredElections } from './modules/updateExpiredElections'
+
+//Cron Job for updating election status automatically
+//Run every 1hour
+cronjob.schedule('0 * * * *', updateExpiredElections)
+
+
 
 app.listen(process.env.PORT, () =>{
     console.log(`Listening on http://localhost:${process.env.PORT}`)
