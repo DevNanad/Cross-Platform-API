2afda7df61d8742e31050d9b60637bce2bf53f7b
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-04-10 23:17:34 +0800

    refactor: send OTP

--- src/handlers/mobileverifyHandler.ts
+++ src/handlers/mobileverifyHandler.ts
@@ -1,25 +1,48 @@
+import prisma from "../db";
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 
-
-export const sendOtp = (req, res) => {
+//SENT OTP 
+export const sendOtp = async (req, res) => {
     try {
-        client.verify.v2.services(process.env.TWILIO_OTP_SERVICE)
-                .verifications
-                .create({to: req.body.mobile_number, channel: 'sms'})
-                .then(verification => {
-                    console.log(verification.status)
-                    res.json({status:verification})
-                });
+        // Check the database if the user number is taken
+        const findNumber = await prisma.user.findUnique({
+            where: { mobile_number: req.body.mobile_number },
+        });
+      
+        // Check if mobile number is already taken
+        if (findNumber) {
+            // Send error message
+            throw new Error("Mobile Number is Taken");
+        } else {
+            // Check if mobile number is valid (should be Philippine number)
+            const lookup = await client.lookups
+            .v2.phoneNumbers(req.body.mobile_number)
+            .fetch();
+            const countryCode = lookup.countryCode;
+            if (countryCode !== "PH") {
+            throw new Error("Invalid phone number. Please provide a valid Philippine phone number.");
+            }
+        
+            // Send OTP
+            const verification = await client.verify
+            .v2.services(process.env.TWILIO_OTP_SERVICE)
+            .verifications.create({ to: req.body.mobile_number, channel: "sms" });
+            console.log(verification.status);
+        
+            // Send success message
+            res.json({ message: "OTP sent to mobile number." });
+        }
     } catch (error) {
-        console.error(error);
-        res.status(401).json({error:error})
+    console.error(error);
+    res.status(400).json({ error: error.message });
     }
+
 }
 
-
+//VERIFY OTP
 export const verifyOtp = (req, res) => {
     try {
         client.verify.v2.services(process.env.TWILIO_OTP_SERVICE)
