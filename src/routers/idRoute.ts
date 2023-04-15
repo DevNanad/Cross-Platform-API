0054e8159ed54aa63ce502561d326b05c904a8bb
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-04-15 21:15:38 +0800

    add validation to single id

--- src/routers/idRoute.ts
+++ src/routers/idRoute.ts
@@ -1,10 +1,12 @@
 import {Router} from 'express'
 import { isAdmin } from '../modules/auth'
 import { singleId } from '../handlers/idHandler'
+import { idSchema } from '../validators/idSchema'
+import { validateRequestSchema } from '../modules/validate-request-schema'
 
 const router = Router()
 
 //upload single id
-router.post('/', isAdmin, singleId)
+router.post('/', idSchema, validateRequestSchema, isAdmin, singleId)
 
 export default router
---
+++ src/validators/idSchema.ts
@@ -0,0 +1,10 @@
+import { check } from "express-validator";
+
+
+const schema = [
+    check('student_id')
+    .exists({checkFalsy: true}).withMessage('Student ID is required')
+    .isLength({min: 7, max: 7}).withMessage('Invalid Student ID')
+]
+
+export {schema as idSchema}
