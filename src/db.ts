7ee797cda4fff6451778855735c0fb9f070b2dab
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-04-25 14:46:41 +0800

    fix: db connection bug

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,20 @@
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient()
 
-export default prisma
+const prisma = new PrismaClient();
+
+
+// Test the database connection on startup
+prisma.$connect().then(() => {
+  console.log('Connected to the database.');
+}).catch((err) => {
+  console.error('Error connecting to the database:', err);
+});
+
+// Handle graceful shutdown
+prisma.$on('beforeExit', async () => {
+  await prisma.$disconnect();
+  console.log('Disconnected from the database.');
+});
+
+export default prisma;
