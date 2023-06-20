92d202a9389d23c88902ce2259cf56b389f94bad
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-06-20 11:25:33 +0800

    add updateStudent Profile validation schema

--- src/validators/updateAdminProfileSchema.ts
+++ src/validators/updateAdminProfileSchema.ts
@@ -9,9 +9,22 @@ const schema = [
     .exists({checkFalsy: true}).withMessage('New Student ID is required')
     .isLength({min: 7, max: 7}).withMessage('Invalid New Student ID'),
 
-    check('fullname')
-    .exists({checkFalsy: true}).withMessage('New Fullname is required')
-    .isString().withMessage("Should be a String")
+    check('firstname')
+    .exists({checkFalsy: true}).withMessage('Firstname is required')
+    .isString().withMessage("Should be a String"),
+
+    check('surname')
+    .exists({checkFalsy: true}).withMessage('Surname is required')
+    .isString().withMessage("Should be a String"),
+
+    check('age')
+    .exists({checkFalsy: true}).withMessage('Age is required')
+    .isLength({min: 1, max: 2}).withMessage('Invalid Age'),
+
+    check('year_level')
+    .exists({ checkFalsy: true }).withMessage('Year Level is required')
+    .isIn(['1st Year', '2nd Year', '3rd Year', '4th Year',]).withMessage('Invalid Year Level. Valid values are "1st Year", "1st Year", "2nd Year" "3rd Year", or "4th Year".'),
+
 ];
 
 export {schema as updateAdminProfileSchema}
