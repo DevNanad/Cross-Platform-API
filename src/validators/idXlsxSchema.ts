1bb32b0952422b96de840ac2f4b6bb3a5cf9f3f0
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-10-11 00:00:37 +0800

    add constraints @ idSlsxSchema validator

--- src/validators/idXlsxSchema.ts
+++ src/validators/idXlsxSchema.ts
@@ -2,14 +2,18 @@ const { body } = require('express-validator');
 
 const schema = [
   body('column')
-    .optional()
-    .isString()
-    .withMessage('Column should be a string'),
+  .optional()
+  .isString()
+  .withMessage('Column should be a string')
+  .matches(/^[A-Za-z]+$/, 'i')
+  .withMessage('Column should only contain letters'),
 
   body('sheet')
     .optional()
     .isInt({ min: 0 })
-    .withMessage('Sheet should be a non-negative integer'),
+    .withMessage('Sheet should be a non-negative integer')
+    .matches(/^\d{1,7}$/, 'i')
+    .withMessage('Sheet should be a 1 to 7 digit integer'),
 ];
 
 export {schema as idXlsxSchema}
