fcd6c15f262227677318459bf199c928bd31b2c9
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-11-13 20:49:09 +0800

    add confirm otp template email

--- src/modules/accountTemplate.ts
+++ src/modules/accountTemplate.ts
@@ -10,5 +10,16 @@ export const newAccountTemplate = (student_id: string, password: string, pin_cod
           <p style="text-align: center;">Login at ${site}/login</p>
       </div>
     </div>`;
-  };
+}
+
+export const confirmationTemplate = (code: string): string => {
+  return `<div style="font-family: Arial, sans-serif; background-color: #f5f5f5; margin: 0; padding: 0;">
+  <div style="max-width: 600px; margin: 0 auto; padding: 20px; background-color: #fff; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+      <h1 style="text-align: center;">One Time Pincode</h1>
+      <p style="text-align: center;">Your confirmation code is:</p>
+      <h1 style="font-size: 24px; text-align: center; color: #333; border: 2px solid #333; border-radius: 5px; padding: 10px; margin: 20px;">${code}</h1>
+      <p style="text-align: center; color: #777;">This code will expire in 10 minutes.</p>
+  </div>
+  </div>`
+}
   
