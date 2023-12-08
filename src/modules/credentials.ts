fe3d53e1ac0dbc7f79da78d07cb8ccb7af46d3b3
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-12-08 19:13:31 +0800

    add CORS headers to credentials module

--- src/modules/credentials.ts
+++ src/modules/credentials.ts
@@ -4,6 +4,9 @@ export const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Credentials', true);
+        res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, authorization')
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Access-Control-Allow-METHODS',"GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH");
     }
     next();
 }
